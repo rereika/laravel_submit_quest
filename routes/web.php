7742fdc9b5cf8e33e235cae54205a8e6d9e8ee13
rereika <rere970807@gmail.com>
2024-06-12 23:31:42 +0900

    feat: 記事関連のルートの追加、ルートの整理

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TagController;
 
-Route::get('/', function () {
-    return view('conduit.home');
-});
+// 既存のルート定義
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Route::get('/editor', function () {
-//     return view('conduit.editor');
-// });
+// 記事関連のルート
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/article/{id}', [ArticleController::class, 'show'])->name('articles.show');
+Route::post('/article', [ArticleController::class, 'store'])->name('articles.store');
+Route::get('/editor', [AuthController::class, 'createArticle'])->name('createArticle');
 
-Route::get('/index', function () {
-    return view('conduit.index');
-});
-
-Route::get('/post', function () {
-    return view('conduit.post');
-});
-
+// その他のルート定義
+Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
 Route::get('/login', [AuthController::class, 'showSigninPage'])->name('login');
 Route::get('/register', [AuthController::class, 'showSignupPage'])->name('register');
-
-Route::get('/editor',[AuthController::class, 'createArticle'])->name('createArticle');
-
-Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
-
-Route::get('/article/how-to-build-webapps-that-scale',[AuthController::class,'viewArticle'])->name('viewArticle');
