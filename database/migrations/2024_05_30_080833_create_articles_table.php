53d92009bd5e5609aa4ca7f53ab2a9cb23666e08
rereika <rere970807@gmail.com>
2024-06-12 23:20:47 +0900

    feat: データベースと連携する

---
+++ database/migrations/2024_05_30_080833_create_articles_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('tags');
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
